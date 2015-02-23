36b5f1f62969f35ad3ec65bb5cab90eba96e4574
Thomas Baer <thomas.baer@tbaer.eu>
2015-02-23 19:04:04 +0100

    First try. Main, args and a show feature

---
+++ examples/ledcli.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import sys
+import getopt
+import max7219.font as font
+import max7219.led as led
+import time
+
+
+def showtext(text, mode, bright):
+    device = led.matrix(cascaded=1)
+    device.flush()
+
+    bright = int(bright)
+    device.brightness(bright)
+    device.show_message(text, font=font.LCD_FONT)
+    device.flush()
+
+def main(argv):
+    text = None
+    mode = 'rtl'
+    bright = '5'
+    try:
+        opts, args = getopt.getopt(argv, "hbt:m", ["text=", "mode=", "bright="])
+    except getopt.GetoptError:
+        print 'ledcli.py -t <text> -m <mode>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'ledcli.py -t <text> -m <mode>'
+            sys.exit()
+        elif opt in ("-t", "--text"):
+            text = arg
+        elif opt in ("-m", "--mode"):
+            mode = arg
+        elif opt in ("-b", "--bright"):
+            bright = arg
+
+    if mode is None:
+        print 'No text given. Stupid.'
+    else:
+        showtext(text, mode, bright)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
